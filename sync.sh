e21310875380175db632f5ee227905b207652167
Jamie Schwinn <jschwinn@computer-talk.com>
2026-08-23 22:24:25 -0400

    sync update

--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 cd ~/coturn/
-git fetch upstream && git rebase upstream/master
+git fetch upstream && git pull upstream master
 cd ~/coturn/build && cmake -S .. -B . -DBUILD_TESTING=ON -DCMAKE_BUILD_TYPE=Release && cmake --build . --parallel "$(nproc)"
 cd ~/coturn && tar -czf coturn-install-$(date +%Y%m%d-%H%M%S).tar.gz install && ls -lh coturn-install-*.tar.gz | tail -n 1
 
